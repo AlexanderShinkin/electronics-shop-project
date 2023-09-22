2fa18f4dff7502668eddb726867c0fd625beba8e
Alexander Shinkin <sh1noprod@gmail.com>
2023-09-22 08:43:03 +0300

    home_work_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
+from src.item import Item
+import pytest
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+@pytest.fixture()
+
+def test_item():
+    """создаем тестовый экземпляр"""
+    return Item("Nokia", 1000, 1)
+
+
+def test_calculate_total_price(test_item):
+    """ тест функции общей стоимости продукции"""
+    assert test_item.calculate_total_price() == 1000
+
+
+def test_apply_discount(test_item):
+    """тест скидки на цену товара"""
+    assert test_item.apply_discount() == 1000
